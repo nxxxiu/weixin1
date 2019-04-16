f07f8b2f8d7901f259bd1d706af1abecab60d544
nxxxiu <1623248430@qq.com>
2019-04-16 11:03:33 +0000

    e

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -269,7 +269,7 @@ class WeixinController extends Controller
 //        print_r($user_list);echo "<br>";die;
         $openid_arr=array_column($user_list,'openid');
 //        print_r($openid_arr);echo "<br>";die;
-        $msg="Everybody, show time~";
+        $msg="stttttttttt";
         $response=$this->sendMsg($openid_arr,$msg);
         echo $response;die;
     }
