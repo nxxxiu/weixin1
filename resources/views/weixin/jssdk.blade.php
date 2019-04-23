e388bc55b54908195b33ede691687fa4266cb00f
nxxxiu <1623248430@qq.com>
2019-04-23 14:46:13 +0000

    图片展示

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -15,7 +15,7 @@
     <img src="" alt="" id="imgs1"  width="300">
     <hr>
     <img src="" alt="" id="imgs2"  width="300">
-    
+
     <script src="/js/jquery/jquery-3.2.1.min.js"></script>
     <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
     <script>
@@ -25,33 +25,33 @@
             timestamp:"{{$jsconfig['timestamp']}}" , // 必填，生成签名的时间戳
             nonceStr:"{{$jsconfig['nonceStr']}}", // 必填，生成签名的随机串
             signature: "{{$jsconfig['signature']}}",// 必填，签名
-            jsApiList: ['chooseImage'] // 必填，需要使用的JS接口列表
+            jsApiList: ['chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
         });
         wx.ready(function () {
             $("#btn1").click(function () {
                 wx.chooseImage({
-                    count: 1, // 默认9
+                    count: 3, // 默认9
                     sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
                     sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                     success: function (res) {
                         var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                       $.each(localIds,function () {
-                           img += v+',';
-                           var node = "#imgs"+i;
-                           $(node).attr('src',v);
+                        $.each(localIds,function () {
+                            img += v+',';
+                            var node = "#imgs"+i;
+                            $(node).attr('src',v);
 
-                           //上传图片
-                           wx.uploadImage({
-                               localId: v, // 需要上传的图片的本地ID，由chooseImage接口获得
-                               isShowProgressTips: 1, // 默认为1，显示进度提示
-                               success: function (res1) {
-                                   var serverId = res1.serverId; // 返回图片的服务器端ID
-                                   //alert('serverID: '+ serverId);
-                                   console.log(res1);
-                               }
-                           });
+                            //上传图片
+                            wx.uploadImage({
+                                localId: v, // 需要上传的图片的本地ID，由chooseImage接口获得
+                                isShowProgressTips: 1, // 默认为1，显示进度提示
+                                success: function (res1) {
+                                    var serverId = res1.serverId; // 返回图片的服务器端ID
+                                    //alert('serverID: '+ serverId);
+                                    console.log(res1);
+                                }
+                            });
 
-                       })
+                        })
 
                         $.ajax({
                             url : '/jssdk/getImg?img='+img,     //将上传的照片id发送给后端
